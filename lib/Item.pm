0a8cb25744d464334f1a3a5d0d8b9820bf4f02b9
Drew Stephens <drew@dinomite.net>
2007-11-14 14:34:17 +0000

    Moving stuff around
    git-svn-id: http://mac-itunes.googlecode.com/svn/trunk@19 77222ab4-f93d-0410-a61e-a52e099c5716

--- lib/Item.pm
+++ lib/Item.pm
@@ -7,13 +7,6 @@ use Carp;
 
 require Exporter;
 our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-# This allows declaration	use Mac::iTunes::Item ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw( );
